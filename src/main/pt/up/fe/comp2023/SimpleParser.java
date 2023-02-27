e8ef536a85cb63deace1c01f76c1500f21dce8ed
Nuno Pereira <nunoafonso2002@gmail.com>
2023-02-27 21:10:59 +0000

    Started using provided grammar, gradually fixing mistakes and improving rules

--- src/main/pt/up/fe/comp2023/SimpleParser.java
+++ src/main/pt/up/fe/comp2023/SimpleParser.java
@@ -42,11 +42,11 @@ public class SimpleParser implements JmmParser {
             // Convert code string into a character stream
             var input = new ANTLRInputStream(jmmCode);
             // Transform characters into tokens using the lexer
-            var lex = new JavammLexer(input);
+            var lex = new pt.up.fe.comp2023.JavammLexer(input);
             // Wrap lexer around a token stream
             var tokens = new CommonTokenStream(lex);
             // Transforms tokens into a parse tree
-            var parser = new JavammParser(tokens);
+            var parser = new pt.up.fe.comp2023.JavammParser(tokens);
 
             // Convert ANTLR CST to JmmNode AST
             return AntlrParser.parse(lex, parser, startingRule)
