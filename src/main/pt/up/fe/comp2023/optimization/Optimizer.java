c7fd89463566d8cc5a746889e110a06fc4214e03
Guilherme Almeida <me@gui1612.com>
2023-05-28 23:20:02 +0100

    revert test stuff

--- src/main/pt/up/fe/comp2023/optimization/Optimizer.java
+++ src/main/pt/up/fe/comp2023/optimization/Optimizer.java
@@ -53,7 +53,6 @@ public class Optimizer implements JmmOptimization {
 
     @Override
     public OllirResult optimize(OllirResult ollirResult) {
-        /*
         int maxRegsAllowed = Integer.parseInt(ollirResult.getConfig().getOrDefault("registerAllocation", "-1"));
 
         if (maxRegsAllowed >= 0) {
@@ -63,7 +62,6 @@ public class Optimizer implements JmmOptimization {
                 ollirResult.getReports().add(new Report(ReportType.ERROR, Stage.OPTIMIZATION, -1, -1, "Register limit exceeded"));
             }
         }
-         */
 
         if (!Boolean.parseBoolean(ollirResult.getConfig().get("optimize")))
             return ollirResult;
